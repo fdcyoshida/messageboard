30f20d7c7efefb0dc133bd23e02d65eeafb0ef18
fdcyoshida <fdc.yoshida@gmail.com>
2024-01-19 17:13:38 +0800

    Up to saving users and adding validation. Error message and password encryption not completed yet

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -11,20 +11,13 @@ class UsersController extends AppController {
         if ($this->request->is('post')) {
             $userData = $this->request->data;
 
-            // バリデーションなどの処理が必要な場合はここで行う
-            CakeLog::write('debug', print_r($postData, true));
-            $this->Session->setFlash('User registration successful.');
-
-            $this->redirect($this->request->referer());
-
-
-            //if ($this->User->save($userData)) {
-                //$this->Session->setFlash('User registration successful.');
-                //$this->redirect($this->request->referer());
-            //} else {
-                //$this->Session->setFlash('User registration failed.');
-            //}
-
+            if ($this->User->save($userData)) {
+                $this->Session->setFlash('User registration successful.');
+                $this->redirect($this->request->referer());
+            } else {
+                $this->Session->setFlash('User registration failed.');
+                $this->redirect($this->request->referer());
+            }
         }
     }
 
--- app/Model/User.php
+++ app/Model/User.php
@@ -2,4 +2,34 @@
 App::uses('AppModel', 'Model');
 
 class User extends AppModel {
+    public $validate = array(
+        'email' => array(
+            'email' => array(
+                'rule' => 'email',
+                'message' => 'Please enter a valid email address format'
+            ),
+            'unique' => array(
+                'rule' => 'isUnique',
+                'message' => 'Email address already in use'
+            )
+        ),
+        'confirm_password' => array(
+            'compare' => array(
+               'rule' => array('comparePasswords'),
+                'message' => 'Passwords do not match',
+                'on' => 'create'
+            )
+        )
+    );
+
+    public function comparePasswords($data) {
+        return ($this->data[$this->alias]['password'] === $data['confirm_password']);
+    }
+
+    //public function beforeSave($options = array()) {
+        //if (isset($this->data[$this->alias]['password'])) {
+            //$this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
+        //}
+        //return true;
+    //}
 }
